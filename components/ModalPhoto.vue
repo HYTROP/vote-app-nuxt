c7743f2417442f8074492f13af43f3c03a72b661
HYTROP <104766532+HYTROP@users.noreply.github.com>
2024-02-23 22:26:48 +0300

    bug-fetch-sheets

--- components/ModalPhoto.vue
+++ components/ModalPhoto.vue
@@ -1,35 +1,30 @@
 <template>
 	<div v-if="show" class="modal">
+		<span class="close" @click="closeModal">×</span>
 		<div class="modal-content">
-			<span class="close" @click="closeModal">×</span>
 			<img :src="imageUrl" class="modal-image" />
-		</div>
-		<div class="infoText">
-			<p class="text-white text-xl">{{ info }}</p>
+
+			<div class="ml-[5%]">
+				<p class="text-white text-md mb-4">
+					{{ props.info[0] }}
+				</p>
+				<p class="text-white text-sm">
+					{{ props.info[3] }}
+				</p>
+				<p class="text-white text-md">
+					{{ props.info[4] }}
+				</p>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-
-const currentPhoto = ref(0);
-
-const images = ref([]);
-
-const showNext = () => {
-	if (currentPhoto.value < images.value.length - 1) {
-		currentPhoto.value++;
-	}
-};
-
-const showPrev = () => {
-	if (currentIndex.value > 0) {
-		currentIndex.value--;
-	}
-};
-
 const props = defineProps({
+	closeModal: {
+		type: Function,
+		default: () => {},
+	},
 	show: {
 		type: Boolean,
 		default: false,
@@ -38,53 +33,35 @@ const props = defineProps({
 		type: String,
 		default: '',
 	},
-
 	info: {
-		type: String,
-		default: '',
+		type: Array,
+		default: () => [],
 	},
 });
-
-onMounted(() => {
-	window.addEventListener('keydown', (event) => {
-		if (event.key === 'ArrowRight') {
-			showNext();
-		} else if (event.key === 'ArrowLeft') {
-			showPrev();
-		}
-	});
-});
-
-const nextImage = () => {
-	showNext();
-};
-
-const prevImage = () => {
-	showPrev();
-};
-
-const { closeModal } = defineEmits(['closeModal']);
 </script>
 
 <style scoped>
-/* Стили для модального окна */
 .modal {
 	position: fixed;
-	z-index: 1;
+	z-index: 2;
 	left: 0;
 	top: 0;
 	width: 100%;
 	height: 100%;
-	background-color: rgba(0, 0, 0, 0.8);
+	background-color: rgba(0, 0, 0, 0.9);
 }
 
 .modal-content {
-	max-width: 80%;
+	max-width: 70%;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	margin: 6% auto;
-	max-height: 1000px;
+	margin: 7% auto;
+}
+
+.modal-image {
+	max-width: 70vw;
+	max-height: 80vh;
 }
 
 .close {
@@ -92,30 +69,16 @@ const { closeModal } = defineEmits(['closeModal']);
 	top: 20px;
 	right: 40px;
 	color: #aaaaaa;
-	font-size: 48px;
+	font-size: 52px;
 	font-weight: bold;
 	cursor: pointer;
 }
-.infoText {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	bottom: 40px;
-	left: 40px;
-	color: #aaaaaa;
-	font-size: 12px;
-}
 
-/* .close:hover,
+.close:hover,
 .close:focus {
 	color: #000;
 	text-decoration: none;
 	cursor: pointer;
-} */
-
-.modal-image {
-	max-width: 90vw;
-	max-height: 80vh;
 }
 
 .modal-content:before,
@@ -126,20 +89,25 @@ const { closeModal } = defineEmits(['closeModal']);
 	top: 50%;
 	width: 30px;
 	height: 30px;
-	background: #ffffff;
+	background: #929292;
 	transform: translateY(-50%);
 	cursor: pointer;
 }
 
 /* Стили для стрелки влево */
 .modal-content:before {
-	left: 10px; /* Установите расположение по вашему усмотрению */
+	width: 40px;
+	height: 40px;
+	left: 40px;
 	clip-path: polygon(0% 50%, 100% 0%, 100% 100%);
+	color: #000;
 }
 
 /* Стили для стрелки вправо */
 .modal-content:after {
-	right: 10px; /* Установите расположение по вашему усмотрению */
+	width: 40px;
+	height: 40px;
+	right: 40px;
 	clip-path: polygon(0% 0%, 100% 50%, 0% 100%);
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,12 @@ import axios from 'axios';
 
 import { _API_KEY, _GOOGLE_SHEETS_KEY, sheetRange } from '../API/Api';
 
-const recordsArr = ref([]);
-const URLs = ref([]);
-const PersonInfo = ref([]);
-const showModal = ref(false);
-const photos = ref([]);
+const recordsArr = ref([]); // Sheets data
+const photos = ref([]); // Drive data
 
-const selectedPhotoId = ref(null);
+const showModal = ref(false); // Modal show
+const selectedPhotoId = ref(null); // Selected photo modal
+const personInfo = ref([]); // Person info for modal
 
 const folderId = '1HTf8I35XQflhavhCRBJSKFqMxZsOLxJu';
 
@@ -20,26 +19,6 @@ const params = {
 };
 // ---------------------
 
-const selectedPhotoUrl = computed(() => {
-	const selectedPhoto = photos.value.find(
-		(photo) => photo.id === selectedPhotoId.value
-	);
-	return selectedPhoto ? getPhotoUrl(selectedPhoto.id) : null;
-});
-const getPhotoUrl = (id) => {
-	return `https://lh3.googleusercontent.com/d/${id}=s2620`;
-};
-
-const openModal = (photoId) => {
-	showModal.value = true;
-	selectedPhotoId.value = photoId;
-};
-
-const closeModal = () => {
-	showModal.value = false;
-	selectedPhotoId.value = null;
-};
-
 onMounted(async () => {
 	try {
 		// -------------------- Google Drive
@@ -48,60 +27,90 @@ onMounted(async () => {
 			{ params }
 		);
 		photos.value = responseDrive.data.files;
+		// console.log(
+		// 	'photos:',
+		// 	photos.value.map((item) => item.id)
+		// );
+	} catch (error) {
+		console.error('Ошибка при получении записей из Google DRIVE:', error);
+	}
 
+	try {
 		// -------------------- Google Sheets
 		const responseSheets = await axios.get(
 			`https://sheets.googleapis.com/v4/spreadsheets/${_GOOGLE_SHEETS_KEY}/values/${sheetRange}?key=${_API_KEY}`
 		);
+
 		recordsArr.value = responseSheets.data.values.map((item) => {
 			const result = item;
 			const separate = result[6].split('/');
 			const nameUrl = decodeURIComponent(separate[separate.length - 1]);
-			const obj = photos.value.find((photo) => photo.name === nameUrl);
-			result[6] = getPhotoUrl(obj.id);
+			// console.log('nameUrl:', nameUrl);
+
+			photos.value.map((photo) => {
+				if (photo.name === nameUrl) {
+					result[6] = getPhotoUrl(photo.id);
+				}
+			});
+
+			// const obj = photos.value.find((photo) => photo.name === nameUrl);
+			// result[6] = getPhotoUrl(obj.id);
+
 			return result;
 		});
-
-		const fullInfoArr = recordsArr.value.map((subArray, index) => {
-			const photoToAdd = photos.value[index];
-			return { ...subArray, ...photoToAdd };
-		});
-		recordsArr.value = fullInfoArr;
-
-		const getUrlFromSheets = recordsArr.value.map((item) => item.id);
-		URLs.value = getUrlFromSheets;
 	} catch (error) {
-		console.error('Ошибка при получении записей из Google :', error);
+		console.error('Ошибка при получении записей из Google SHEETS:', error);
 	}
 });
+
+const getPhotoUrl = (id) => {
+	return `https://lh3.googleusercontent.com/d/${id}=s2620`;
+};
+
+const openModal = (photoId) => {
+	// record[6] inside foo
+	showModal.value = true;
+	selectedPhotoId.value = photoId;
+	const findPerson = recordsArr.value.find((item) => item[6] === photoId);
+	personInfo.value = findPerson;
+};
+
+const closeModal = () => {
+	showModal.value = false;
+	selectedPhotoId.value = null;
+};
 </script>
 
 <template>
 	<div>
-		<div class="top-0 left-[10%] w-full fixed bg-neutral-200/5 h-20 z-10">
+		<div class="top-0 left-[10%] fixed h-20 z-10">
 			<h1 class="text-2xl font-bold m-6">ГАЛЕРЕЯ /</h1>
 		</div>
 
 		<div
 			class="grid grid-flow-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10 m-20"
 			v-auto-animate>
-			<div v-for="record in recordsArr" :key="record" class="group block">
-				<img
-					:src="record[6]"
-					@click="openModal(record[6])"
-					class="shadow-2xl hover:scale-110 transition duration-400 mb-3 group-hover:stroke-white" />
-				<p
-					class="active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
-					{{ record[0] }}
-				</p>
+			<div v-for="record in recordsArr" :key="record">
+				<div class="flex items-center justify-center">
+					<img
+						:src="record[6]"
+						@click="openModal(record[6])"
+						class="shadow-2xl hover:scale-110 transition duration-400 mb-3 group-hover:stroke-white"
+						alt="photo" />
+				</div>
+				<div>
+					<p class="text-sm m-3 text-neutral-700">
+						{{ record[0] }}
+					</p>
+					<p class="text-sm m-3 text-neutral-500">({{ record[4] }})</p>
+				</div>
 			</div>
 
 			<ModalPhoto
-				:photos="recordsArr"
+				:info="personInfo"
 				:show="showModal"
 				:imageUrl="selectedPhotoId"
-				:info="PersonInfo"
-				@click="closeModal" />
+				:closeModal="closeModal" />
 		</div>
 	</div>
 </template>
