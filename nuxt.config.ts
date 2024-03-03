7b936729125f864277398d12779c485c2c710374
HYTROP <104766532+HYTROP@users.noreply.github.com>
2024-03-04 01:59:59 +0300

    try vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	buildModules: ['nuxt-vite'],
 	modules: [
 		'@formkit/auto-animate/nuxt',
 		'@nuxtjs/tailwindcss',
