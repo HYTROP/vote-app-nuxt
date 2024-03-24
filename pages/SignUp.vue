6061a0c528c45e0671b907317ae0c8fbd2105eef
HYTROP <104766532+HYTROP@users.noreply.github.com>
2024-03-24 14:34:27 +0300

    rating + filter = demo okok

--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -97,7 +97,7 @@
 								<div class="text-green-600 animate__animated animate__fadeIn">
 									{{ successMsg }}
 								</div>
-								<LoaderSpin />
+
 								<button
 									type="submit"
 									class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-gray-600"
