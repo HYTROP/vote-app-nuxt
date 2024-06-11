d9a385e0211bb2261865c7e005c4e96308132a8f
HYTROP <104766532+HYTROP@users.noreply.github.com>
2024-06-11 17:44:43 +0300

    increase sizes pagi

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -6,7 +6,7 @@
 			<a
 				href="#"
 				@click.prevent="previousPage"
-				class="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+				class="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-md font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
 			>
 				<ArrowLongLeftIcon
 					class="mx-1 h-6 w-12 text-gray-400 hover:text-indigo-600 hover:scale-125 transition duration-300 ease-in-out"
@@ -24,7 +24,7 @@
 						? 'border-indigo-500  text-indigo-600 transition-all duration-300'
 						: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
 				"
-				class="inline-flex items-center border-t-2 cursor-pointer border-transparent px-5 pt-5 text-sm font-medium"
+				class="inline-flex items-center border-t-2 cursor-pointer border-transparent px-5 pt-5 text-md font-medium"
 				@click="currentPage = page"
 				>{{ page }}
 			</span>
@@ -34,7 +34,7 @@
 			<a
 				@click.prevent="nextPage"
 				href="#"
-				class="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+				class="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-md font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
 			>
 				<ArrowLongRightIcon
 					class="mx-1 h-6 w-12 text-gray-400 hover:text-indigo-600 hover:scale-125 transition duration-300 ease-in-out"
